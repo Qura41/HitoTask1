3fb8320475620f9b459e203943c3fadf77b71851
Qura <sunopium2@gmail.com>
2025-05-08 23:14:40 +0300

    Методы: - Создать массив. - Вывести массив на экран. - Скопировать массив. - Перевернуть массив. - Повернуть массив на 90 градусов против часовой стрелки. - Повернуть массив на 90 градусов по часовой стрелке.

---
+++ HitoTaskArray1/ArrayMethods.cs
@@ -0,0 +1,117 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace HitoTaskArray1;
+
+public class ArrayMethods
+{
+        // Вывести массив на экран.
+        public static void PrintArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // Скопировать существующий массив в новый.
+        public static int[,] CopyArray(int[,] array)
+        {
+            int[,] copiedArray = new int[array.GetLength(0), array.GetLength(1)];
+            int lines = copiedArray.GetLength(0);
+            int columns = copiedArray.GetLength(1);
+            
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    copiedArray[i, j] = array[i, j];
+                }
+            }
+
+            return copiedArray;
+        }
+        
+        // Создать двумерный массив на основе заданных параметров.
+        public static int[,] CreateArray(int linesCount, int columnsCount)
+        {
+            Random random = new Random();
+            
+            int[,] arrayNumbers = new int[linesCount, columnsCount];
+            for (int i = 0; i < linesCount; i++)
+            {
+                for (int j = 0; j < columnsCount; j++)
+                {
+                    arrayNumbers[i, j] = random.Next(1, 10);
+                }
+            }
+            
+            return arrayNumbers;
+        }
+
+        // Перевернуть массив.
+        public static int[,] ReverseArray(int[,] array)
+        {
+            int[,] reversedArray = CopyArray(array);
+            int lines = reversedArray.GetLength(0);
+            int columns = reversedArray.GetLength(1);
+
+
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j <= columns / 2; j++)
+                {
+                    int temp = reversedArray[i, j];
+                    reversedArray[i, j] = reversedArray[i, columns - 1 - j];
+                    reversedArray[i, columns - 1 - j] = temp;
+                }
+            }
+            
+            return reversedArray;
+        }
+
+        // Повернуть массив на 90° градусов против часовой стрелки.
+        public static int[,] Rotate90Counterclockwise(int[,] array)
+        {
+            int[,] rotatedArray = CopyArray(array);
+            int lines = rotatedArray.GetLength(0);
+            int columns = rotatedArray.GetLength(1);
+
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    int linesRotated = j;
+                    int columnsRotated = lines - (i + 1);
+
+                    rotatedArray[columnsRotated, linesRotated] = array[j, i];
+                }
+            }
+
+            return rotatedArray;
+        }
+        
+        // Повернуть массив на 90 градусов по часовой стрелке.
+        public static int[,] Rotate90Clockwise(int[,] array)
+        {
+            int[,] rotatedArray = CopyArray(array);
+            int lines = rotatedArray.GetLength(0);
+            int columns = rotatedArray.GetLength(1);
+
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    int linesRotated = columns - (j + 1);
+                    int columnsRotated = i;
+
+                    rotatedArray[columnsRotated, linesRotated] = array[i, j];
+                }
+            }
+
+            return rotatedArray;
+        }
+}
---
+++ HitoTaskArray1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices.ComTypes;
+using static HitoTaskArray1.ArrayMethods;
+
+namespace HitoTaskArray1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] finalArray;
+            
+            Console.WriteLine("Введите количество строк:");
+            int linesCount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество элементов в каждой строке:");
+            int columnsCount = int.Parse(Console.ReadLine());
+            
+            int[,] array = CreateArray(linesCount, columnsCount);
+            
+            PrintArray(array);
+            
+            Console.WriteLine("\nВыберите действие:\n1. Повернуть массив против часовой стрелке.\n2. Повернуть массив по часовой стрелки.\n3. Инверсия массива.");
+            int answer = int.Parse(Console.ReadLine());
+
+            switch (answer)
+            {
+                case 1:
+                    Console.WriteLine("\nПоворот: против часовой стрелки.\nПовернуть на:\n1. 90°\n2. 180°\n3. 270°");
+                    int answer1 = int.Parse(Console.ReadLine());
+                    switch (answer1)
+                    {
+                        case 1:
+                            finalArray = Rotate90Counterclockwise(array);
+                            PrintArray(finalArray);
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        default:
+                            Console.WriteLine("\nПоворот: по часовой стрелке.\nНеправильный ввод, дальнейшие действия недоступны.");
+                            break;
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("\nПовернуть на:\n1. 90°\n2. 180°\n3. 270°");
+                    int answer2 = int.Parse(Console.ReadLine());
+                    switch (answer2)
+                    {
+                        case 1:
+                            finalArray = Rotate90Clockwise(array);
+                            PrintArray(finalArray);
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        default:
+                            Console.WriteLine("\nНеправильный ввод, дальнейшие действия недоступны.");
+                            break;
+                    }
+                    break;
+                case 3:
+                    finalArray = ReverseArray(array);
+                    PrintArray(finalArray);
+                    Console.WriteLine("Массив перевернут.");
+                    break; 
+                default:
+                    Console.WriteLine("\nНеправильный ввод, дальнейшие действия недоступны.");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
